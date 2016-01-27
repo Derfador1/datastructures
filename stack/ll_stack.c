17357be13bb54b4ad33e4d02ce44b8dc4d637a2b
Liam Echlin <wechlin@umbctraining.com>
2016-01-27 13:36:54 -0500

    Create initital stack type

---
+++ stack/ll_stack.c
@@ -0,0 +1,25 @@
+
+#include "stack.h"
+
+stack *stack_create(void)
+{
+	stack *s = malloc(sizeof(*s));
+	if(s) {
+		s->head = NULL;
+	}
+
+	return s;
+}
+
+void stack_destroy(stack *s)
+{
+	if(!s) {
+		return;
+	}
+	ll_destroy(s->head);
+	free(s);
+}
+
+bool stack_is_empty(stack *s);
+void stack_push(stack *s, double data);
+double stack_pop(stack *s);
