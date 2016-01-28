6370b8758fbf93d9ae1d6ad7e6091671d2c1fc9c
Liam Echlin <wechlin@umbctraining.com>
2016-01-28 12:09:31 -0500

    Print enqueued items

--- queue/driver.c
+++ queue/driver.c
@@ -11,5 +11,9 @@ int main(void)
 	queue_enqueue(queuey, 51.3);
 	queue_enqueue(queuey, 89.99999);
 
+	while(!queue_is_empty(queuey)) {
+		printf("%lf\n", queue_dequeue(queuey));
+	}
+
 	queue_destroy(queuey);
 }
