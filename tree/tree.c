fe96dbd143a17e7373009f5d675bdc84f27b3d4f
Liam Echlin <wechlin@umbctraining.com>
2016-01-29 10:36:00 -0500

    Add destroy and first iterate functions for tree

--- tree/tree.c
+++ tree/tree.c
@@ -15,5 +15,24 @@ struct tree *tree_create(double data)
 
 void tree_destroy(struct tree *t)
 {
+	if(!t) {
+		return;
+	}
 
+	tree_destroy(t->left);
+	tree_destroy(t->right);
+	free(t);
 }
+
+
+void tree_iterate(struct tree *t, void (*func)(double))
+{
+	if(!t) {
+		return;
+	}
+
+	tree_iterate(t->left, func);
+	tree_iterate(t->right, func);
+	func(t->data);
+}
+
