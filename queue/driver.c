4b100ed8c203b7ec581335223c53ddbfd62faebf
Liam Echlin <wechlin@umbctraining.com>
2016-01-29 09:20:28 -0500

    Demonstrate flatten queue function

--- queue/driver.c
+++ queue/driver.c
@@ -7,9 +7,16 @@ int main(void)
 {
 	queue *queuey = queue_create();
 
-	queue_enqueue(queuey, 82.1);
-	queue_enqueue(queuey, 51.3);
-	queue_enqueue(queuey, 89.99999);
+	queue_enqueue(queuey, 89);
+	queue_enqueue(queuey, 82);
+	queue_enqueue(queuey, 51);
+	queue_enqueue(queuey, 89);
+	queue_enqueue(queuey, 82);
+	queue_enqueue(queuey, 82);
+	queue_enqueue(queuey, 51);
+	queue_enqueue(queuey, 82);
+
+	queue_flatten(&queuey);
 
 	while(!queue_is_empty(queuey)) {
 		printf("%lf\n", queue_dequeue(queuey));
