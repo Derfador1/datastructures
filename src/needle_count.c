32162cb948d299dcc1e7fcbf0cd9088cfd4eead1
Liam Echlin <wechlin@umbctraining.com>
2016-02-04 15:20:27 -0500

    Fixed the needle_count function so that it actually works

--- src/needle_count.c
+++ src/needle_count.c
@@ -21,7 +21,6 @@ size_t data_size(void)
 
 size_t needle_count(const char *needle, const char *arr[], size_t n)
 {
-
 	size_t start = 0;
 	size_t end = n - 1;
 
@@ -38,25 +37,41 @@ size_t needle_count(const char *needle, const char *arr[], size_t n)
 		// find the transition point between our needle
 		// and "not-our-needle"
 		if(result <= 0) {
+			if(idx == 0) {
+				break;
+			}
 			high = idx - 1;
 		} else if(result > 0) {
+			if(idx == end) {
+				break;
+			}
 			low = idx + 1;
 		}
+	}
+	// If the needle would lie outside the array, it
+	// ain't there
+	if(low > end || high > end) {
+		return 0;
 	}
 	// If our markers end on the same spot, they are either
 	// on first instance or just to the left of the first
 	// instance
+	// TODO: Remove the the low == high possibility
 	if(low == high) {
 		if(0 == strcmp(needle, arr[low])) {
 			start = low;
-		} else if(0 == strcmp(needle, arr[low + 1])) {
+		} else if(low < end && 0 == strcmp(needle, arr[low + 1])) {
 			start = low + 1;
+		} else {
+			return 0;
 		}
 	} else {
 		if(0 == strcmp(needle, arr[low])) {
 			start = low;
 		} else if(0 == strcmp(needle, arr[high])) {
 			start = high;
+		} else {
+			return 0;
 		}
 	}
 	// TODO: What happens if the needle doesn't exist?
@@ -84,13 +99,13 @@ size_t needle_count(const char *needle, const char *arr[], size_t n)
 	// If our markers end on the same spot, they are either
 	// on first instance or just to the left of the first
 	// instance
+
 	if(low == high) {
 		if(0 == strcmp(needle, arr[high])) {
 			end = high;
 		} else if(0 == strcmp(needle, arr[high - 1])) {
 			end = high - 1;
 		}
-		// TODO: What happens if the needle doesn't exist?
 	} else {
 		if(0 == strcmp(needle, arr[low])) {
 			end = low;
@@ -109,6 +124,8 @@ int main(void)
 	printf("There are %zu lights\n", data_size());
 	printf("Happy  %zu (20)\n", needle_count("Happy", data, data_size()));
 	printf("Sleepy %zu (1)\n", needle_count("Sleepy", data, data_size()));
+	printf("Edward %zu (0)\n", needle_count("Edward", data, data_size()));
 	printf("Tyrion %zu (0)\n", needle_count("Tyrion", data, data_size()));
+	printf("Aquman %zu (0)\n", needle_count("Aquman", data, data_size()));
 
 }
