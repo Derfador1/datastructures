8def1d411364c97734a45869b6096a8f000f1bcc
Liam Echlin <wechlin@umbctraining.com>
2016-02-04 13:32:35 -0500

    Create initial draft of needle_count function

--- src/needle_count.c
+++ src/needle_count.c
@@ -1,6 +1,6 @@
 
 #include <stdio.h>
-#include <stlib.h>
+#include <stdlib.h>
 #include <string.h>
 
 const char *data[] = {
@@ -21,14 +21,85 @@ size_t data_size(void)
 
 size_t needle_count(const char *needle, const char *arr[], size_t n)
 {
-	return 0;
+
+	size_t start = 0;
+	size_t end = n - 1;
+
+	// Find the start
+	size_t low = start;
+	size_t high = end;
+
+	while(low < high) {
+		size_t idx = low + (high - low)/2;
+		int result = strcmp(needle, arr[idx]);
+
+		// If we found the needle we are looking for,
+		// we need to keep looking backward until we
+		// find the transition point between our needle
+		// and "not-our-needle"
+		if(result <= 0) {
+			high = idx - 1;
+		} else if(result > 0) {
+			low = idx + 1;
+		}
+	}
+	// If our markers end on the same spot, they are either
+	// on first instance or just to the left of the first
+	// instance
+	if(low == high) {
+		if(0 == strcmp(needle, arr[low])) {
+			start = low;
+		} else if(0 == strcmp(needle, arr[low + 1])) {
+			start = low + 1;
+		}
+		// TODO: What happens if the needle doesn't exist?
+	} else {
+		printf("high(%zu), low(%zu)\n", high, low);
+	}
+
+
+
+	low = start;
+	high = end;
+
+	// Find the end
+	while(low < high) {
+		size_t idx = low + (high - low)/2;
+		int result = strcmp(needle, arr[idx]);
+
+		// If we found the needle we are looking for,
+		// we need to keep looking forward until we
+		// find the transition point between our needle
+		// and "not-our-needle"
+		if(result < 0) {
+			high = idx - 1;
+		} else if(result >= 0) {
+			low = idx + 1;
+		}
+	}
+	// If our markers end on the same spot, they are either
+	// on first instance or just to the left of the first
+	// instance
+	if(low == high) {
+		if(0 == strcmp(needle, arr[low])) {
+			end = low;
+		} else if(0 == strcmp(needle, arr[low + 1])) {
+			end = low + 1;
+		}
+		// TODO: What happens if the needle doesn't exist?
+	} else {
+		printf("end: high(%zu), low(%zu)\n", high, low);
+	}
+
+
+	return end - start + 1;
 }
 
 int main(void)
 {
 
-	printf("Happy %d (20)\n", needle_count("Happy", data, data_size()));
-	printf("Sleepy %d (1)\n", needle_count("Sleepy", data, data_size()));
-	printf("Tyrion %d (0)\n", needle_count("Tyrion", data, data_size()));
+	printf("Happy  %zu (20)\n", needle_count("Happy", data, data_size()));
+	printf("Sleepy %zu (1)\n", needle_count("Sleepy", data, data_size()));
+	printf("Tyrion %zu (0)\n", needle_count("Tyrion", data, data_size()));
 
 }
