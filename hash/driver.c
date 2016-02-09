eb2081e0ecae67318015ed00340e46d004651a72
Liam Echlin <wechlin@umbctraining.com>
2016-02-09 10:44:21 -0500

    Show collision resolution for hashes

--- hash/driver.c
+++ hash/driver.c
@@ -10,10 +10,20 @@ int main(void)
 	hash_insert(hashy, "able", 13);
 	hash_insert(hashy, "baker", 97);
 	hash_insert(hashy, "charlie",2);
+	hash_insert(hashy, "(neato)", 18);
+	hash_insert(hashy, "chef", 2000);
 
 
+	printf("able: %lf\n",
+			hash_fetch(hashy, "able"));
 	printf("baker: %lf\n",
 			hash_fetch(hashy, "baker"));
+	printf("charlie: %lf\n",
+			hash_fetch(hashy, "charlie"));
+	printf("(neato): %lf\n",
+			hash_fetch(hashy, "(neato)"));
+	printf("chef: %lf\n",
+			hash_fetch(hashy, "chef"));
 
 
 	hash_destroy(hashy);
