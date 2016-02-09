d21871775fe98405efeb569bdabc3d82d1bb6a07
Liam Echlin <wechlin@umbctraining.com>
2016-02-09 11:43:30 -0500

    Add demonstration of reallocing hash

--- hash/driver.c
+++ hash/driver.c
@@ -12,6 +12,14 @@ int main(void)
 	hash_insert(hashy, "charlie",2);
 	hash_insert(hashy, "(neato)", 18);
 	hash_insert(hashy, "chef", 2000);
+	hash_insert(hashy, "dickerson", 2003);
+	hash_insert(hashy, "eggbert", 2005);
+	hash_insert(hashy, "follensbee", 2600);
+	hash_insert(hashy, "graham", 2090);
+	hash_insert(hashy, "hillock", 2200);
+	hash_insert(hashy, "igloo", 2031);
+	hash_insert(hashy, "jamiroquai", 2900);
+	hash_insert(hashy, "killua", 2080);
 
 
 	printf("able: %lf\n",
@@ -24,6 +32,8 @@ int main(void)
 			hash_fetch(hashy, "(neato)"));
 	printf("chef: %lf\n",
 			hash_fetch(hashy, "chef"));
+
+	printf("capacity: %zu\n", hashy->capacity);
 
 
 	hash_destroy(hashy);
