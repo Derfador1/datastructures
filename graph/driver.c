e21f6f38a0cf59d27dce4161b45d2f8b07391423
Liam Echlin <wechlin@umbctraining.com>
2016-02-15 10:44:42 -0500

    Add graph demo

---
+++ graph/driver.c
@@ -0,0 +1,32 @@
+
+
+#include <stdio.h>
+
+#include "graph.h"
+
+void print_item(const void *data, bool is_node)
+{
+	if(is_node) {
+		printf("\n%c", (char)data);
+	} else {
+		printf(u8" → %c", (char)data);
+	}
+}
+
+int main(void)
+{
+	graph *graphy = graph_create();
+
+	graph_add_node(graphy, (void *)'A');
+	graph_add_node(graphy, (void *)'B');
+	graph_add_edge(graphy, (void *)'B', (void *)'A', 5);
+	graph_add_node(graphy, (void *)'C');
+	graph_add_node(graphy, (void *)'D');
+	graph_add_edge(graphy, (void *)'B', (void *)'D', 5);
+	graph_add_edge(graphy, (void *)'D', (void *)'B', 5);
+	graph_add_edge(graphy, (void *)'C', (void *)'A', 5);
+
+	graph_print(graphy, print_item);
+
+	graph_disassemble(graphy);
+}
