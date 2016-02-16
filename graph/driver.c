f07ad577684ba7232b5f563dde3c91670d1fa03a
Liam Echlin <wechlin@umbctraining.com>
2016-02-16 10:40:46 -0500

    Convert all nodes to strings instead of characters

--- graph/driver.c
+++ graph/driver.c
@@ -9,9 +9,9 @@
 void print_item(const void *data, bool is_node)
 {
 	if(is_node) {
-		printf("\n%c", (char)data);
+		printf("\n%s", (char *)data);
 	} else {
-		printf(u8" → %c", (char)data);
+		printf(u8" → %s", (char *)data);
 	}
 }
 
@@ -22,30 +22,32 @@ struct llist *graph_path(const graph *g, const void *from, const void *to)
 	hash_insert(visited, from, NULL); 
 
 	hash_disassemble(visited);
+
+	return NULL;
 }
 
 int main(void)
 {
 	graph *graphy = graph_create();
 
-	graph_add_node(graphy, (void *)'A');
-	graph_add_node(graphy, (void *)'B');
-	graph_add_edge(graphy, (void *)'B', (void *)'A', 5);
-	graph_add_node(graphy, (void *)'C');
-	graph_add_node(graphy, (void *)'D');
-	graph_add_edge(graphy, (void *)'B', (void *)'D', 5);
-	graph_add_edge(graphy, (void *)'D', (void *)'B', 5);
-	graph_add_edge(graphy, (void *)'C', (void *)'A', 5);
-	graph_add_node(graphy, (void *)'E');
+	graph_add_node(graphy, (void *)"A");
+	graph_add_node(graphy, (void *)"B");
+	graph_add_edge(graphy, (void *)"B", (void *)"A", 5);
+	graph_add_node(graphy, (void *)"C");
+	graph_add_node(graphy, (void *)"D");
+	graph_add_edge(graphy, (void *)"B", (void *)"D", 5);
+	graph_add_edge(graphy, (void *)"D", (void *)"B", 5);
+	graph_add_edge(graphy, (void *)"C", (void *)"A", 5);
+	graph_add_node(graphy, (void *)"E");
 
 	graph_print(graphy, print_item);
 
 	printf("\n%zu nodes, %zu edges\n", graph_node_count(graphy), graph_edge_count(graphy));
 
-	struct llist *path = graph_path(graphy, (void *)'D', (void *)'A');
+	struct llist *path = graph_path(graphy, (void *)"D", (void *)"A");
 	struct llist *curr = path;
 	while(curr) {
-		printf(u8"%c → ", (char)(curr->data));
+		printf(u8"%s → ", (char *)(curr->data));
 		curr = curr->next;
 	}
 	printf("\n");
