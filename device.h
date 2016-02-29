3e68c3a163510fa3c2c462b1faacb6aa883f9a57
Derfador1 <Derfador1@gmail.com>
2016-02-29 15:24:50 -0500

    header for device.h

---
+++ device.h
@@ -0,0 +1,12 @@
+#ifndef DEVICE_H
+	#define DEVICE_H
+
+struct device {
+	double battery_power;
+	double longitude;
+	double latitude;
+	float altitude;
+	unsigned int source_dev_id; 	
+};
+
+#endif
