fdb97099842ee1701933b6671198ee31ed9cfeca
Derfador1 <Derfador1@gmail.com>
2016-02-29 14:44:54 -0500

    Fixed hash code errors

--- hash/hash.h
+++ hash/hash.h
@@ -11,11 +11,11 @@ hash *hash_create(void);
 void hash_destroy(hash *h);
 void hash_disassemble(hash *h);
 
-bool hash_insert(hash *h, int key, void *value);
-bool hash_remove(hash *h, int key);
+bool hash_insert(hash *h, const char *key, void *value);
+bool hash_remove(hash *h, const char *key);
 
-bool hash_exists(hash *h, int key);
-void *hash_fetch(hash *h, int key);
+bool hash_exists(hash *h, const char *key);
+void *hash_fetch(hash *h, const char *key);
 
 void hash_traverse(hash *h, void (*func)(const char *, void *));
 
